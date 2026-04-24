ac80b44b6eb7fb765c35d55a59ced9889c1dce4e
marekdurak <durakmar@fel.cvut.cz>
2026-04-24 18:41:38 +0300

    Update site configuration with new author and details

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'aed369a7ea5f82b488ad81162f032015',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Marek Durák',
+  domain: 'marekdurak.com',
+  author: 'Marek Durák',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Embedded Software Engineer & Hardware Designer Portfolio',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
